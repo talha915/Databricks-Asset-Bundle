697a9ee5c3d67fcee3794fae5e4db0e629dd39e2
talha915 <talhazafar915426@gmail.com>
2026-07-04 16:45:38 +0200

    adding config.py

--- dlt_pipeline/src/common/config.py
+++ dlt_pipeline/src/common/config.py
@@ -1,2 +1,18 @@
-/Volumes/dev/bara_dataset/bara_dataset/extracted/sql-data-warehouse-project/datasets/source_crm/cust_info.csv
+# Configurations
 
+source_catalog = "ai_lab_demo"
+source_schema = "source"
+source_volume = "source_volume"
+
+# Folders
+crm_folder = f"/Volumes/{source_catalog}/{source_schema}/{source_volume}/source_crm"
+erp_folder = f"/Volumes/{source_catalog}/{source_schema}/{source_volume}/source_erp"
+
+# File Paths
+crm_cust_info = f"{crm_folder}/cust_info.csv"
+crm_prd_info = f"{crm_folder}/prd_info.csv"
+crm_sales_details = f"{crm_folder}/sales_details.csv"
+
+erp_cust = f"{erp_folder}/CUST_AZ12.csv"
+erp_loc = f"{erp_folder}/LOC_A101.csv"
+erp_px_cat = f"{erp_folder}/PX_CAT_G1V2.csv"
