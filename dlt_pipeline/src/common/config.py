6be02c739d4b66f3909e73cf091ac9da40d0d87e
talha915 <talhazafar915426@gmail.com>
2026-07-06 00:02:02 +0200

    bronze pipeline

--- dlt_pipeline/src/common/config.py
+++ dlt_pipeline/src/common/config.py
@@ -17,15 +17,15 @@ erp_folder = f"/Volumes/{source_catalog}/{source_schema}/{source_volume}/source_
 # erp_loc = f"{erp_folder}/LOC_A101.csv"
 # erp_px_cat = f"{erp_folder}/PX_CAT_G1V2.csv"
 
-source_file_map = {
-    "crm_cust_info": f"{crm_folder}/cust_info.csv",
-    "crm_prd_info": f"{crm_folder}/prd_info.csv",
-    "crm_sales_details": f"{crm_folder}/sales_details.csv",
+# source_file_map = {
+#     "crm_cust_info": f"{crm_folder}/cust_info.csv",
+#     "crm_prd_info": f"{crm_folder}/prd_info.csv",
+#     "crm_sales_details": f"{crm_folder}/sales_details.csv",
 
-    "erp_cust": f"{erp_folder}/CUST_AZ12.csv",
-    "erp_loc": f"{erp_folder}/LOC_A101.csv",
-    "erp_px_cat": f"{erp_folder}/PX_CAT_G1V2.csv"
-}
+#     "erp_cust": f"{erp_folder}/CUST_AZ12.csv",
+#     "erp_loc": f"{erp_folder}/LOC_A101.csv",
+#     "erp_px_cat": f"{erp_folder}/PX_CAT_G1V2.csv"
+# }
 
 
 # Target
@@ -43,7 +43,7 @@ def get_check_paths(layer: str) -> dict:
     base = f"/Volumes/{target_catalog}/{layer}/{checkpoint_volume}"
     return {
         "checkpoint": f"{base}/checkpoints",
-        "schema_checkpoint_path": f"{base}/schemas"
+        "schema_location": f"{base}/schemas"
     }
 
 bronze_check_path = get_check_paths(target_schema_bronze)
