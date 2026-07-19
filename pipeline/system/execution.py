ddf1ad00f2fc138c572d3520860bc89887bf58bc
talha915 <talhazafar915426@gmail.com>
2026-07-19 19:31:47 +0200

    file and folder structure

--- pipeline/system/execution.py
+++ pipeline/system/execution.py
@@ -3,6 +3,7 @@ from framework.utils import get_spark
 
 spark = get_spark()
 
+
 def start_run(layer, job_name):
 
     run_id = int(datetime.now().timestamp())
@@ -27,14 +28,16 @@ def start_run(layer, job_name):
 
 def end_run(run_id, status, records=0, error=None):
 
+    error_value = "NULL" if error is None else f"'{error}'"
+
     spark.sql(f"""
     UPDATE ai_lab_demo.system.execution_log
 
     SET
-    status='{status}',
-    end_time=current_timestamp(),
-    records={records},
-    error='{error}'
+        status='{status}',
+        end_time=current_timestamp(),
+        records={records},
+        error={error_value}
 
     WHERE run_id={run_id}
 
