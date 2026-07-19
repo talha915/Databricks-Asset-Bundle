df445b4b94a1bcf7e099209c44e7e2b87451f6bc
talha915 <talhazafar915426@gmail.com>
2026-07-19 17:53:27 +0200

    file and folder structure

---
+++ pipeline/framework/config.py
@@ -0,0 +1,8 @@
+BRONZE_DB = "bronze"
+
+VOLUME_PATH = "/Volumes/ai_lab_demo/dataset/ai_lab_demo_src"
+
+employees_src_a = f"{VOLUME_PATH}/employees/sourceA"
+employees_src_b = f"{VOLUME_PATH}/employees/sourceB"
+employees_src_c = f"{VOLUME_PATH}/employees/sourceC"
+departments = f"{VOLUME_PATH}/departments"
---
+++ pipeline/system/execution.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+
+def start_run(layer, job_name):
+
+    run_id = int(datetime.now().timestamp())
+
+    spark.sql(f"""
+    INSERT INTO execution_logs
+    VALUES(
+        {run_id},
+        '{layer}',
+        '{job_name}',
+        'RUNNING',
+        current_timestamp(),
+        NULL,
+        0,
+        NULL
+    )
+    """)
+
+    return run_id
+
+
+
+def end_run(run_id, status, records=0, error=None):
+
+    spark.sql(f"""
+    UPDATE execution_logs
+
+    SET
+    status='{status}',
+    end_time=current_timestamp(),
+    records={records},
+    error='{error}'
+
+    WHERE run_id={run_id}
+
+    """)
